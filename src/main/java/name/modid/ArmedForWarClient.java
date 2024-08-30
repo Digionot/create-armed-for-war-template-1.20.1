7c1bce260ae66c5ae6109abe8ca28762a775ee35
Digionot <digionot@gmail.com>
2024-08-30 16:58:25 -0400

    first commit

---
+++ src/main/java/name/modid/ArmedForWarClient.java
@@ -0,0 +1,11 @@
+package name.modid;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ArmedForWarClient implements ClientModInitializer{
+
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
